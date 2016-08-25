2fc3de84d554f515d35b50be5d36b478dc534360
lawrencetung <lawrence.c.tung@gmail.com>
2016-08-25 14:24:55 -0700

    first change. Still need to fix folder.

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -5,9 +5,9 @@ namespace PJS.Bootstrap {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
 
-            manifest.DefineScript("Bootstrap").SetUrl("bootstrap-3.3.5/js/bootstrap.min.js", "bootstrap-3.3.5/js/bootstrap.js").SetVersion("3.3.4").SetDependencies("jQuery");
+            manifest.DefineScript("Bootstrap").SetUrl("bootstrap/js/bootstrap.min.js", "bootstrap/js/bootstrap.js").SetVersion("3.3.7").SetDependencies("jQuery");
             manifest.DefineScript("HoverDropdown").SetUrl("hover-dropdown.js").SetDependencies("Bootstrap");
-            manifest.DefineScript("Stapel-Modernizr").SetUrl("stapel/modernizr.custom.63321.js");
+            manifest.DefineScript("Stapel-Modernizr").SetUrl("stapel/modernizr.custom.js");
             manifest.DefineScript("Stapel").SetUrl("stapel/jquery.stapel.js").SetDependencies("jQuery", "Stapel-Modernizr");
             manifest.DefineScript("prettyPhoto").SetUrl("prettyPhoto/jquery.prettyPhoto.js").SetDependencies("jQuery");
             manifest.DefineScript("Custom").SetUrl("custom.js").SetDependencies("jQuery");
